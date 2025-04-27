da37660a5507e1a3ebf5bc14f6fad2c3ff611a7d
winnieotieno <winnieotieno770@gmail.com>
2025-04-27 18:02:14 +0300

    created html folder and changed the paths

--- script/script.js
+++ script/script.js
@@ -1,6 +1,8 @@
 // Mobile Navigation
 const hamburger = document.querySelector(".hamburger")
 const navLinks = document.querySelector(".nav-links")
+console.log("Hamburger clicked");
+
 
 
 if (hamburger) {
